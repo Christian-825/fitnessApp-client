71642373272317e3e2789228eb90f80043e97d27
chan12nievera <chan12nievera@gmail.com>
2025-08-05 13:44:25 +0800

    Fix workouts status

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -44,7 +44,7 @@ export default function Workouts() {
         </div>
 
         {workouts.length === 0 ? (
-          <div className="text-center text-light">
+          <div className="text-start text-light">
             <p>No workouts yet.</p>
           </div>
         ) : (
