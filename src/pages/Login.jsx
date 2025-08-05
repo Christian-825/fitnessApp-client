74b4b9f5bea9552aea399a468b29c00f56414bed
chan12nievera <chan12nievera@gmail.com>
2025-08-05 14:51:11 +0800

    Fix login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
         
         {/* Left Column - Welcome Message or Image */}
         <div className="col-md-6 d-flex flex-column justify-content-center align-items-center text-light p-5 ">
-          <h1 className="display-4 fw-bold text-center">Welcome!</h1>
+          <h1 className="display-4 fw-bold text-center">Begin Now!</h1>
           <p className="lead text-center">Log in or sign up to start your fitness journey.</p>
         </div>
 
